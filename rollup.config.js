4a23fbf81dc4f64d422f2f96d131604aa2ffd14b
Hendrik Erz <hendrik@zettlr.com>
2025-06-12 11:53:59 +0200

    chore: Open entry file on server start

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,10 @@ import dev from 'rollup-plugin-dev'
 import commonjs from '@rollup/plugin-commonjs'
 import svg from 'rollup-plugin-svg'
 import json from '@rollup/plugin-json'
+import open from 'open'
+
+const DEV_SERVER_HOST = '127.0.0.1'
+const DEV_SERVER_PORT = '8080'
 
 export default defineConfig({
   input: 'src/main.ts',
@@ -20,7 +24,14 @@ export default defineConfig({
     nodeResolve(), // Resolves imports and requires
     commonjs(), // Necessary for the `hash-sum` package
     json(), // Support JSON file loading
-    dev(), // Dev server support
+    dev({
+      host: DEV_SERVER_HOST,
+      port: DEV_SERVER_PORT,
+      onListen (server) {
+        // Upon server start, load the main entry file
+        open(`http://${DEV_SERVER_HOST}:${DEV_SERVER_PORT}/index.htm`)
+      }
+    }), // Dev server support
     svg() // Import SVG files (icons) into the bundle
   ]
 })
