f3ff7ad0cd1394da68271f9f1cc3a3df748912e3
Hendrik Erz <hendrik@zettlr.com>
2025-06-13 15:38:51 +0200

    fix: Push dist

--- dist/conferia.js
+++ dist/conferia.js
@@ -8636,7 +8636,7 @@
         return { toolbar, filter, personalAgendaToggle, toIcalButton, fullscreenButton };
     }
 
-    var version = "0.5.0";
+    var version = "0.6.0";
     var pkg = {
     	version: version};
 
